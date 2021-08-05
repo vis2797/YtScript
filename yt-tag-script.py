5439100dd07c9188676b4ce81ef9e58ed14f6d41
vis2797 <sharmavishal929@gmail.com>
2021-08-05 23:33:47 +0530

    completed project

---
+++ yt-tag-script.py
@@ -0,0 +1,24 @@
+import sys
+
+#To get input filepath as a cmd line agrument.
+infilepath = str(sys.argv[1])
+
+#To get input output filepath as a cmd line agrument.
+outfilepath = str(sys.argv[2])
+
+#To open a desired file in read mode.
+fin=open(infilepath,'r')
+
+#To open a file to write into it.
+fout=open(outfilepath,'w')
+
+#imports content line by line.
+line=fin.readline()
+while line:
+	#implementation of comma at the end of every line in the file.
+	fout.write("{},\n".format( line.strip()))
+	line = fin.readline()
+
+
+fin.close()
+fout.close()
